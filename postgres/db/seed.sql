355a603f5af6e9fe18304ddbfbf2131ae7d4a733
Attila Domokos <adomokos@gmail.com>
2017-03-09 20:03:58 -0600

    Load seed data when container created

---
+++ postgres/db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO members(first_name, last_name, email) VALUES ('John', 'Lennon', 'john@appleltd.com');
+INSERT INTO members(first_name, last_name, email) VALUES ('Paul', 'McCartney', 'paul@appleltd.com');
--- postgres/scripts/init_docker_postgres.sh
+++ postgres/scripts/init_docker_postgres.sh
@@ -1,10 +1,10 @@
-
 # this script is runned when the docker container is built
 # it imports the base database structure and create the database for the tests
 
 DATABASE_NAME="kashmir"
 DATABASE_USER="kashmir_user"
-DB_DUMP_LOCATION="/tmp/psql_data/structure.sql"
+DB_SCHEMA="/tmp/psql_data/structure.sql"
+DB_SEED="/tmp/psql_data/seed.sql"
 
 echo "*** CREATING DATABASE ***"
 
@@ -16,6 +16,8 @@ psql -v ON_ERROR_STOP=1 -U postgres <<EOSQL
 EOSQL
 
 # import sql_dump
-psql -v ON_ERROR_STOP=1 -U "$DATABASE_USER" -d "$DATABASE_NAME" -f "$DB_DUMP_LOCATION"
+psql -v ON_ERROR_STOP=1 -U "$DATABASE_USER" -d "$DATABASE_NAME" -f "$DB_SCHEMA"
+# seed the DB
+psql -v ON_ERROR_STOP=1 -U "$DATABASE_USER" -d "$DATABASE_NAME" -f "$DB_SEED"
 
 echo "*** DATABASE CREATED! ***"
