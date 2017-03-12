70c8c0d28d2ea209964d5d355dadf4d6b3147840
Attila Domokos <adomokos@gmail.com>
2017-03-12 15:45:30 -0500

    Test out redis connection from Ruby instance

--- ruby/app/hello.rb
+++ ruby/app/hello.rb
@@ -1,3 +1,7 @@
 require 'faker'
+require 'redis'
 
 puts "Hello #{Faker::Name.name}!"
+redis = Redis.new(:host => "172.18.0.2", :port => 6379, :db => 15)
+redis.set("docker", "awesome")
+puts "Docker is #{redis.get("docker")}"
